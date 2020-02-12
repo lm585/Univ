e6d12d0964e7373e441153b98a4703816485791d
lm585 <lm585@users.noreply.github.com>
2020-02-12 13:50:59 -0500

    Add files via upload

---
+++ script-2-R-t-test.CGsites.meth.RAvsRO.R
@@ -0,0 +1,69 @@
+#
+#TN.w	RA.w	RARO.w	TEM.w	TN.c	RA.c	RARO.c	TEM.c
+#1	0.8	0.714286	0.64	0.9	0.857143	0.714286	0.6
+
+
+
+ls | grep "^ENSG" | while read f 
+do 
+ wc -l $f; 
+done | awk '
+ {
+  if($1 > 0)
+    print $2;
+ }
+ ' > temp-ENSG-files
+
+cat temp-ENSG-files genes-constant-meth-t-test-error.RAvRO.genes.txt  | sort | uniq -u > temp-441pm
+mv temp-441pm temp-ENSG-files
+
+cat temp-ENSG-files |  while read file
+do
+ echo "name <- c(\"$file\")";
+ cut -f 2 $file > temp-s1
+ cut -f 6 $file >> temp-s1
+ cut -f 3 $file > temp-s2
+ nl=`wc -l temp-s2 | awk '{print $1}'`
+ cut -f 7 $file >> temp-s2
+ echo "nl = $nl"
+ 
+ cat temp-s1 | awk '
+ BEGIN{
+  FS = "\t";
+  OFS = "";
+  ORS = "";
+      }
+ {
+  if(NR == 1)
+  {
+   print "a <- c(";
+  }
+  print $1 ", "
+ }
+ END{print "\n"}' | sed 's/, $/)/'
+
+ cat temp-s2 | awk '
+ BEGIN{
+  FS = "\t";
+  OFS = "";
+  ORS = "";
+      }
+ {
+  if(NR == 1)
+  {
+   print "b <- c(";
+  }
+  print $1 ", "
+ }
+ END{print "\n"}' | sed 's/, $/)/'
+
+ echo "cor <- t.test(a, b, paired = TRUE)";
+ echo "name";
+ echo "nl";
+ echo "mean1 <- mean(a)"
+ echo "mean2 <- mean(b)"
+ echo "cor\$p.value";
+ echo "mean1";
+ echo "mean2";
+ echo "cor\$estimate";
+done
